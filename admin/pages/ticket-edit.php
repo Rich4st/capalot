0388108b2ca2ae2dfa995822abc49632c5902fb6
Rich4st <zmingcheng204@gmail.com>
2023-09-21 16:36:03 +0800

    新增: 后台工单回复;

--- admin/pages/ticket-edit.php
+++ admin/pages/ticket-edit.php
@@ -2,13 +2,11 @@
 
 defined('WPINC') || exit;
 
-// ticket-edit.php
-
 $ticket_id = absint($_GET['id']);
 $Data = Capalot_Ticket::get($ticket_id);
 
 if (empty($Data)) {
-	wp_die('获取工单数据失败。请返回重试');
+    wp_die('获取工单数据失败。请返回重试');
 }
 
 $creator_user = ($user = get_user_by('ID', $Data->creator_id)) ? $user->user_login : '';
@@ -19,31 +17,30 @@ $statuses = array(
     '3' => Capalot_Ticket::get_status(3)
 );
 
-if ( isset( $_POST['save_record'] ) ) {
+if (isset($_POST['save_record'])) {
 
-	check_admin_referer( 'rimini_edit_nonce_action', 'rimini_edit_nonce_val' );
+    check_admin_referer('rimini_edit_nonce_action', 'rimini_edit_nonce_val');
 
-	$status = intval($_POST['status']);
+    $status = intval($_POST['status']);
 
-	if ($Data->status < 2 && !empty($_POST['reply_content'])) {
-		$status = 2;
-	}
+    if ($Data->status < 2 && !empty($_POST['reply_content'])) {
+        $status = 2;
+    }
 
-	$updata = [
-		'reply_content'=> wp_kses_post($_POST['reply_content']),
-		'reply_time'=> time(),
-		'updated_time'=> time(),
-		'assignee_id'=> get_current_user_id(),
-		'status'=> $status
-	];
+    $update = [
+        'reply_content' => wp_kses_post($_POST['reply_content']),
+        'reply_time' => time(),
+        'updated_time' => time(),
+        'assignee_id' => get_current_user_id(),
+        'status' => $status
+    ];
 
-	if (Capalot_Ticket::update($updata,['id'=>$Data->id])) {
-		$message = '工单更新成功';
-		$Data = Capalot_Ticket::get($ticket_id);
-	}else{
-		$message = '工单更新失败';
-	}
-
+    if (Capalot_Ticket::update($update, ['id' => $Data->id])) {
+        $message = '工单更新成功';
+        $Data = Capalot_Ticket::get($ticket_id);
+    } else {
+        $message = '工单更新失败';
+    }
 }
 
 $current_status = $Data->status;
@@ -54,14 +51,16 @@ $current_status = $Data->status;
 
 <!-- 主页面 -->
 <form method="post">
-    <?php wp_nonce_field( 'rimini_edit_nonce_action', 'rimini_edit_nonce_val' ); ?>
+    <?php wp_nonce_field('rimini_edit_nonce_action', 'rimini_edit_nonce_val'); ?>
     <div class="wrap">
         <h1 class="wp-heading-inline">编辑/回复工单</h1>
         <a class="add-new-h2" href="admin.php?page=capalot-admin-ticket">返回工单列表</a>
-        <?php if (!empty($message)) {echo '<div class="notice notice-zbinfo is-dismissible" id="message"><p>' . $message . '</p></div>';}?>
+        <?php if (!empty($message)) {
+            echo '<div class="notice notice-zbinfo is-dismissible" id="message"><p>' . $message . '</p></div>';
+        } ?>
         <hr class="wp-header-end">
         <div id="poststuff">
-            
+
             <div id="post-body" class="metabox-holder columns-2">
                 <div id="post-body-content" class="edit-form-section edit-comment-section">
                     <div id="namediv" class="stuffbox">
@@ -72,19 +71,21 @@ $current_status = $Data->status;
                                     <tbody>
                                         <tr>
                                             <td class="first"><label>工单创建人</label></td>
-                                            <td><input type="text" size="30" value="<?php echo $creator_user;?>" disabled></td>
+                                            <td><input type="text" size="30" value="<?php echo $creator_user; ?>" disabled></td>
                                         </tr>
                                         <tr>
                                             <td class="first"><label>工单类型</label></td>
-                                            <td><input type="text" size="30" value="<?php echo Capalot_Ticket::get_type($Data->type);?>" disabled></td>
+                                            <td><input type="text" size="30" value="<?php echo Capalot_Ticket::get_type($Data->type); ?>" disabled></td>
                                         </tr>
                                         <tr>
                                             <td class="first"><label>问题标题：</label></td>
-                                            <td><?php echo $Data->title;?></td>
+                                            <td><?php echo $Data->title; ?></td>
                                         </tr>
                                         <tr>
                                             <td class="first"><label>问题内容：</label></td>
-                                            <td><div style=" background: #eee; padding: 10px; "><?php echo $Data->content;?></div></td>
+                                            <td>
+                                                <div style=" background: #eee; padding: 10px; "><?php echo $Data->content; ?></div>
+                                            </td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -92,20 +93,20 @@ $current_status = $Data->status;
                         </div>
                     </div>
                     <div id="postdiv" class="postarea">
-                    	<p>回复内容：</p>
-	                    <?php
-						$content = $Data->reply_content;
-						$editor_id = 'reply_content'; // 自定义编辑器 ID
-						$settings = array(
-						    'textarea_name' => 'reply_content', // 这个 textarea 的 name 属性值
-						    'media_buttons' => false, // 是否显示添加媒体按钮
-						    'tinymce'       => false, // 是否开启 TinyMCE 编辑器
-						    'quicktags'     => true, // 是否显示快速标签工具栏
-						    'editor_height' => 200, // 是否显示快速标签工具栏
-						);
-						wp_editor( $content, $editor_id, $settings );
-						?>
-					</div>
+                        <p>回复内容：</p>
+                        <?php
+                        $content = $Data->reply_content;
+                        $editor_id = 'reply_content'; // 自定义编辑器 ID
+                        $settings = array(
+                            'textarea_name' => 'reply_content', // 这个 textarea 的 name 属性值
+                            'media_buttons' => false, // 是否显示添加媒体按钮
+                            'tinymce'       => false, // 是否开启 TinyMCE 编辑器
+                            'quicktags'     => true, // 是否显示快速标签工具栏
+                            'editor_height' => 200, // 是否显示快速标签工具栏
+                        );
+                        wp_editor($content, $editor_id, $settings);
+                        ?>
+                    </div>
                 </div><!-- /post-body-content -->
                 <div id="postbox-container-1" class="postbox-container">
                     <div id="submitdiv" class="stuffbox">
@@ -114,19 +115,19 @@ $current_status = $Data->status;
                             <div class="submitbox">
                                 <div>
                                     <div>
-                                    	<div class="misc-pub-section">
-                                    		<p style=" margin: 5px 0; ">创建时间：<b><?php echo (!empty($Data->create_time)) ? wp_date('Y-m-d H:i:s', $Data->create_time) : '';?></b></p>
-                                    		<p style=" margin: 5px 0; ">回复时间：<b><?php echo (!empty($Data->reply_time)) ? wp_date('Y-m-d H:i:s', $Data->reply_time) : '';?></b></p>
-                                    		<p style=" margin: 5px 0; ">更新时间：<b><?php echo (!empty($Data->updated_time)) ? wp_date('Y-m-d H:i:s', $Data->updated_time) : '';?></b></p>
-										</div>
                                         <div class="misc-pub-section">
-                                        	当前状态： <span style=" margin-bottom: 10px; display: inline-block; "><?php echo Capalot_Ticket::get_status($current_status); ?></span>
+                                            <p style=" margin: 5px 0; ">创建时间：<b><?php echo (!empty($Data->create_time)) ? wp_date('Y-m-d H:i:s', $Data->create_time) : ''; ?></b></p>
+                                            <p style=" margin: 5px 0; ">回复时间：<b><?php echo (!empty($Data->reply_time)) ? wp_date('Y-m-d H:i:s', $Data->reply_time) : ''; ?></b></p>
+                                            <p style=" margin: 5px 0; ">更新时间：<b><?php echo (!empty($Data->updated_time)) ? wp_date('Y-m-d H:i:s', $Data->updated_time) : ''; ?></b></p>
+                                        </div>
+                                        <div class="misc-pub-section">
+                                            当前状态： <span style=" margin-bottom: 10px; display: inline-block; "><?php echo Capalot_Ticket::get_status($current_status); ?></span>
                                             <fieldset>
-                                                <?php foreach ( $statuses as $value => $label ) : ?>
-							                    	<label style=" margin-bottom: 10px; display: inline-block; ">
-							                        <input type="radio" name="status" value="<?php echo $value; ?>" <?php checked( $current_status, $value ); ?>><?php echo $label; ?>
-							                        </label><br>
-							                    <?php endforeach; ?>
+                                                <?php foreach ($statuses as $value => $label) : ?>
+                                                    <label style=" margin-bottom: 10px; display: inline-block; ">
+                                                        <input type="radio" name="status" value="<?php echo $value; ?>" <?php checked($current_status, $value); ?>><?php echo $label; ?>
+                                                    </label><br>
+                                                <?php endforeach; ?>
                                             </fieldset>
                                         </div>
                                     </div>
@@ -134,7 +135,8 @@ $current_status = $Data->status;
                                 </div>
                                 <div id="major-publishing-actions">
                                     <div id="publishing-action">
-                                        <input type="submit" name="save_record" class="button button-primary button-large" value="更新工单"></div>
+                                        <input type="submit" name="save_record" class="button button-primary button-large" value="更新工单">
+                                    </div>
                                     <div class="clear"></div>
                                 </div>
                             </div>
