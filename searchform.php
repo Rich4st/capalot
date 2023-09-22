a25c2112be00936bcd06353d3520849b97a85c18
pinglunzy <1104020730@qq.com>
2023-09-22 15:18:13 +0800

    新增：头部搜索表单

--- searchform.php
+++ searchform.php
@@ -1,26 +1,37 @@
+<div class="search-form absolute inset-0 bg-white border-2 rounded-md  z-50 dark:bg-dark-card dark:border-[#252529]">
+    <div class=" h-full grid content-center">
+        <div class=" px-2">
+            <form method="get" action="<?php echo esc_url(home_url('/')); ?>">
+                <div class=" flex flex-row   ">
+                    <?php if (_capalot('is_site_pro_search', true)) : ?>
+                        <div class="search-select w-1/6 self-center">
+                            <div class=" flex justify-center ">
+                                <?php
+                                wp_dropdown_categories(array(
+                                    'hide_empty'       => false,
+                                    'show_option_none' => __('全站', 'ripro'),
+                                    'option_none_value' => '',
+                                    'order'          => 'DESC',
+                                    'orderby'          => _capalot('pro_search_select_order', 'id'),
+                                    'hierarchical'     => true,
+                                    'depth'     => intval(_capalot('pro_search_select_depth', 1)),
+                                    'id'     => 'cat-search-select',
+                                    'class'     => 'form-select dark:bg-dark-card dark:text-gray-400 outline-none',
+                                )); ?>
+                            </div>
+                        </div>
+                    <?php endif; ?>
 
-<div class="search-form">
-    <form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-        <?php if (_capalot('is_site_pro_search',true)) : ?>
-            <div class="search-select">
-                <?php 
-                wp_dropdown_categories( array(
-                    'hide_empty'       => false,
-                    'show_option_none' => __('全站','ripro'),
-                    'option_none_value' => '',
-                    'order'          => 'DESC',
-                    'orderby'          => _capalot('pro_search_select_order','id'),
-                    'hierarchical'     => true,
-                    'depth'     => intval(_capalot('pro_search_select_depth',1)),
-                    'id'     => 'cat-search-select',
-                    'class'     => 'form-select',
-                ) );?>
-            </div>
-        <?php endif; ?>
-
-        <div class="search-fields">
-          <input type="text" class="" placeholder="<?php _e('输入关键词 回车...', 'ripro'); ?>" autocomplete="off" value="<?php echo esc_attr( get_search_query() ) ?>" name="s" required="required">
-          <button title="点击搜索" type="submit"><i class="fas fa-search"></i></button>
-      </div>
-  </form>
+                    <div class="search-fields  w-5/6">
+                        <div class=" flex flex-row px-2">
+                            <div class=" w-5/6"><input type="text" class=" text-lg py-2 px-2 w-full outline-none dark:bg-dark-card dark:text-gray-400" placeholder="<?php _e('输入关键词 回车...', 'ripro'); ?>" autocomplete="off" value="<?php echo esc_attr(get_search_query()) ?>" name="s" required="required"></div>
+                            <div class=" w-1/6 self-center ">
+                                <div class=" flex justify-end hover:opacity-70"><button title="点击搜索" type="submit"><i class="fas  fa-search "></i></button></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
