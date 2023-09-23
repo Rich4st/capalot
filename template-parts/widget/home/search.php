62585c4aca2fb4dfe3a0d1c76d2461f4dbe3b09b
pinglunzy <1104020730@qq.com>
2023-09-23 17:15:37 +0800

    新增：移动端导航，修复头部/首页搜索

--- template-parts/widget/home/search.php
+++ template-parts/widget/home/search.php
@@ -80,12 +80,54 @@ switch ($type) {
 ?>
 
 
-<div id="<?php echo $vanta_uniqid; ?>" class="vanta-bg <?php echo $v_class; ?> relative text-white text-center h-96">
+<div id="<?php echo $vanta_uniqid; ?>" class="vanta-bg <?php echo $v_class; ?> relative  text-center h-96">
 
   <div class="search-wrap absolute w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 space-y-4">
-    <h2 class="text-3xl font-semibold"><?php echo $args['title']; ?></h2>
-    <p><?php echo $args['desc']; ?></p>
-    <?php get_search_form(); ?>
+    <h2 class="text-3xl font-semibold text-white"><?php echo $args['title']; ?></h2>
+    <p class=" text-white"><?php echo $args['desc']; ?></p>
+
+
+    <div class=" max-w-[60rem] m-auto lg:px-0 px-4">
+      <div class="search-form   bg-white border-2 rounded-md   dark:bg-dark-card dark:border-[#252529]">
+        <div class=" h-full grid content-center">
+          <div class=" px-2">
+            <form method="get" action="<?php echo esc_url(home_url('/')); ?>">
+              <div class=" flex flex-row   ">
+                <?php if (_capalot('is_site_pro_search', true)) : ?>
+                  <div class="search-select w-1/6 self-center">
+                    <div class=" flex justify-center ">
+                      <?php
+                      wp_dropdown_categories(array(
+                        'hide_empty'       => false,
+                        'show_option_none' => __('全站', 'ripro'),
+                        'option_none_value' => '',
+                        'order'          => 'DESC',
+                        'orderby'          => _capalot('pro_search_select_order', 'id'),
+                        'hierarchical'     => true,
+                        'depth'     => intval(_capalot('pro_search_select_depth', 1)),
+                        'id'     => 'cat-search-select',
+                        'class'     => 'form-select dark:bg-dark-card dark:text-gray-400 outline-none',
+                      )); ?>
+                    </div>
+                  </div>
+                <?php endif; ?>
+
+                <div class="search-fields  w-5/6">
+                  <div class=" flex flex-row px-2">
+                    <div class=" w-5/6"><input type="text" class=" text-lg py-2 px-2 w-full outline-none dark:bg-dark-card dark:text-gray-400" placeholder="<?php _e('输入关键词 回车...', 'ripro'); ?>" autocomplete="off" value="<?php echo esc_attr(get_search_query()) ?>" name="s" required="required"></div>
+                    <div class=" w-1/6 self-center ">
+                      <div class=" flex justify-end hover:opacity-70 dark:text-gray-400"><button title="点击搜索" type="submit"><i class="fas  fa-search "></i></button></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
 
     <?php if (!empty($search_hot_exp)) : ?>
       <ul class="space-x-1 flex justify-center items-center text-sm text-gray-300">
