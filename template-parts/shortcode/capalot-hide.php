dd26fa3eb680fd5d0e6757430ff32347a551e144
SmileyZhou <92788507+Adszhou@users.noreply.github.com>
2023-09-25 13:00:35 +0800

    修改：付费内容暗黑模式样式问题

--- template-parts/shortcode/capalot-hide.php
+++ template-parts/shortcode/capalot-hide.php
@@ -11,13 +11,13 @@ $is_user_login_get_status = $user_pay_post_status === '0' && empty($user_id);
 
 ?>
 
-<div class=" p-4 border-dashed border-2 border-rose-200 rounded-md my-4 bg-gray-100 overflow-hidden relative dark:bg-dark dark:border-gray-600">
+<div class=" p-4 border-dashed border-2 border-rose-200 rounded-md my-4 bg-gray-100 overflow-hidden relative dark:bg-dark-card dark:border-gray-600">
   <?php if ($user_pay_post_status && !$is_user_login_get_status) : ?>
     <div class="absolute right-0 top-0 px-2 bg-rose-300 text-white z-40 rounded-tr-md rounded-bl-md"><span class=" text-sm  "><?php echo '已获得查看权限'; ?></span></div>
     <?php echo $content; ?>
   <?php else : ?>
-    <span class="font-bold text-[18px] leading-6 relative pl-[18px] border-l-[#3370e9] border-l-2"><?php echo '隐藏内容'; ?></span>
-    <div class="my-2 border-2 border-dashed border-[#ffb1cb] rounded-[0.5rem] py-[1.5rem] px-[1rem] text-center block">
+    <span class="absolute right-0 top-0 px-2 bg-rose-300 text-white z-40 rounded-tr-md rounded-bl-md"><?php echo '隐藏内容'; ?></span>
+    <div class="my-2  py-[1.5rem] px-[1rem] text-center block">
       <?php if ($is_user_login_get_status) : ?>
         <div class="text-[#ff5722] text-[1.25rem] mb-[1rem] flex justify-center items-center"><i class="fas fa-lock me-1"></i><?php _e('本内容登录后免费查看', 'capalot'); ?></div>
         <div class="text-[#ff5722] text-[1.25rem] mb-[1rem] flex justify-center items-center leading-[1.5rem]">
@@ -34,7 +34,7 @@ $is_user_login_get_status = $user_pay_post_status === '0' && empty($user_id);
           </button>
         </div>
 
-        <div class="text-[0.95rem] p-[0.5rem] leading-6 md:w-[550px]  w-full bg-[#eaf6ff] text-center block my-0 mx-[auto]">
+        <div class="text-[0.95rem] p-[0.5rem] leading-6 md:w-[550px] rounded w-full bg-[#eaf6ff] text-center block my-0 mx-[auto] dark:bg-[#293745]">
 
           <ul class="">
             <?php
@@ -61,9 +61,9 @@ $is_user_login_get_status = $user_pay_post_status === '0' && empty($user_id);
                 $__price_span = '<span>' . __('免费', 'capalot') . '</span>';
               } elseif ($coin_price < $default_price) {
                 $__rate = $coin_price / $default_price * 10;
-                $__price_span = '<span class="text-[#f7c32e]"><i class="fas ' . '金币' . ' me-1"></i>' . $coin_price . '金币' . '<sup class="ms-1">' . sprintf(__('%s折', 'capalot'), $__rate) . '<sup></span>';
+                $__price_span = '<span class=""><i class="fas' . '金币' . ' me-1"></i>' . $coin_price . '金币' . '<sup class="ms-1 ">' . sprintf(__('%s折', 'capalot'), $__rate) . '<sup></span>';
               } else {
-                $__price_span = '<span><span class="dashicons dashicons-database text-[1rem]"></span><i class="fas ' . '金币' . ' me-1"></i>' . $coin_price . '金币' . '</span>';
+                $__price_span = '<span class=" text-[#f7c32e] dark:text-[#f7c32e]"><span class="dashicons dashicons-database text-[1rem] "></span><i class="fas ' . '金币' . ' me-1"></i>' . $coin_price . '金币' . '</span>';
               }
 
               echo '<li class="inline-block px-2  ' . $type . '">' . $price_names[$type] . ': ' . $__price_span . '</li>';
@@ -75,7 +75,7 @@ $is_user_login_get_status = $user_pay_post_status === '0' && empty($user_id);
         <?php
         $sales_count = absint(get_post_meta($post_id, 'capalot_paynum', true));
         if ($sales_count > 0) {
-          echo '<div class="text-[#8d9da9] text-[0.9rem] mt-[1rem]"><i class="fab fa-hotjar me-1"></i>' . sprintf(__('已有<span class="text-[#ff5722] mx-[2px] text-[0.875rem]">%d</span>人解锁查看', 'capalot'), $sales_count) . '</div>';
+          echo '<div class="text-[#8d9da9] text-[0.9rem] mt-[1rem] "><i class="fab fa-hotjar me-1"></i>' . sprintf(__('已有<span class="text-[#ff5722] mx-[2px] text-[0.875rem]">%d</span>人解锁查看', 'capalot'), $sales_count) . '</div>';
         }
         ?>
       <?php endif; ?>
