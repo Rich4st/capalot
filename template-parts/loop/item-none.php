2ef66a35007a5be2dc14fba97e8fe7ed78497cf6
Rich4st <zmingcheng204@gmail.com>
2023-09-27 11:59:14 +0800

    样式: 暂无内容样式修改;

--- template-parts/loop/item-none.php
+++ template-parts/loop/item-none.php
@@ -1,5 +1,3 @@
-<div class=" col-span-4 text-center">
-    <div class=" py-16 text-xl text-gray-500">
-        <i class="fas fa-mug-hot me-2"></i><?php _e('暂无内容', 'ripro'); ?>
-    </div>
-</div>
+<p class=" py-16 text-xl text-gray-500 col-span-4 text-center">
+    <i class="fa-solid fa-box-open fa-lg mr-2"></i>暂无内容
+</p>
