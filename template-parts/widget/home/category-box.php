cd0bfba9af1a2fe2f92325a02e5b79eb0e5c4a27
Rich4st <zmingcheng204@gmail.com>
2023-10-08 09:25:06 +0800

    修复: 分类BOX模块暗黑模式样式;

--- template-parts/widget/home/category-box.php
+++ template-parts/widget/home/category-box.php
@@ -35,34 +35,37 @@ $def_bg_img = get_template_directory_uri() . '/assets/img/bg.jpg'; //默认缩�
 
 ?>
 
+<section class="dark:bg-dark">
 
-<section class="swiper mySwiper max-w-7xl mx-auto dark:bg-dark" data-config='<?php echo json_encode($config); ?>'>
-  <div class="swiper-wrapper">
+  <div class="swiper mySwiper max-w-7xl mx-auto" data-config='<?php echo json_encode($config); ?>'>
+    <div class="swiper-wrapper">
 
-    <?php foreach ($terms as $key => $item) :
+      <?php foreach ($terms as $key => $item) :
 
-      $meta_bg = get_term_meta($item->term_id, 'bg-image', true);
+        $meta_bg = get_term_meta($item->term_id, 'bg-image', true);
 
-      $bg_img = (!empty($meta_bg)) ? $meta_bg : $def_bg_img;
+        $bg_img = (!empty($meta_bg)) ? $meta_bg : $def_bg_img;
 
-      $color = capalot_get_color_class($key);
-    ?>
+        $color = capalot_get_color_class($key);
+      ?>
 
-      <div class="swiper-slide py-8">
-        <div class="h-40 flex justify-center items-center text-white text-center" style="background-image: url(<?php echo $bg_img; ?>);">
+        <div class="swiper-slide py-8">
+          <div class="h-40 flex justify-center items-center text-white text-center" style="background-image: url(<?php echo $bg_img; ?>);">
 
-          <a href="<?php echo get_term_link($item->term_id, $taxonomy_name); ?>">
-            <div>
-              <?php if (!empty($args['is_num'])) : ?>
-                <span class="bg-<?php echo $color; ?> bg-accent text-sm p-1 rounded-lg"><?php echo $item->count; ?>+</span>
-              <?php endif; ?>
-              <h3 class="font-semibold mt-1 text-xl"><?php echo $item->name; ?></h3>
-            </div>
-          </a>
+            <a href="<?php echo get_term_link($item->term_id, $taxonomy_name); ?>">
+              <div>
+                <?php if (!empty($args['is_num'])) : ?>
+                  <span class="bg-<?php echo $color; ?> bg-accent text-sm p-1 rounded-lg"><?php echo $item->count; ?>+</span>
+                <?php endif; ?>
+                <h3 class="font-semibold mt-1 text-xl"><?php echo $item->name; ?></h3>
+              </div>
+            </a>
 
+          </div>
         </div>
-      </div>
 
-    <?php endforeach; ?>
+      <?php endforeach; ?>
+    </div>
   </div>
+
 </section>
