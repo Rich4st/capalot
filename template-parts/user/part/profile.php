8fbf54b1dcf84848a045963dd62635f5fe553432
SmileyZhou <92788507+Adszhou@users.noreply.github.com>
2023-10-09 13:48:00 +0800

    样式：快捷登录绑定信息样式

--- template-parts/user/part/profile.php
+++ template-parts/user/part/profile.php
@@ -82,8 +82,8 @@ if ($unsetoauth == 'qq' || $unsetoauth == 'weixin') {
     <div class="lg:col-span-4 col-span-1 col-start-1">
       <?php if (_capalot('is_sns_qq', false) || _capalot('is_sns_weixin', false)) : ?>
         <div class="relative my-4">
-          <hr class="">
-          <p class="small text-sm bg-white absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 -right-1/2 px-2"><?php _e('快捷登录绑定信息', 'ripro'); ?></p>
+          <hr>
+          <p class="text-sm text-center absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 -right-1/2 px-2"><?php _e('快捷登录绑定信息', 'ripro'); ?></p>
         </div>
         <div class="grid gap-4 md:block py-4  text-white text-center">
 
